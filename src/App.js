0638077f457c3698c087806e417613e51ce39052
junaid-ash42 <junaid.ashraf4275@gmail.com>
2026-09-23 22:44:55 +0500

    feat: add ParticleTextStudio component and improve image export fallback handling in CodeCardStudio

--- src/App.js
+++ src/App.js
@@ -39,12 +39,14 @@ import BPMTapper from "./components/BPMTapper";
 import CaesarCipher from "./components/CaesarCipher";
 import NumberGuessingGame from "./components/NumberGuessingGame";
 import KeycodeDetector from "./components/KeycodeDetector";
+import ParticleTextStudio from "./components/ParticleTextStudio";
 import CodeCardStudio from "./components/CodeCardStudio";
 
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-50 pb-10">
+      <ParticleTextStudio />
       <CodeCardStudio />
       <SortingVisualizer />
       <SpeedClicker />
